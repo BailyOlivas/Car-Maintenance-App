0a1da0645334171e18afbc5c621a48c33cab13e0
BailyOlivas <bailyolivas8@gmail.com>
2025-03-05 23:28:34 -0600

    Added name to Users database
    Also added welcome "name" within dashboard view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_08_034412) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_06_051903) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -64,6 +64,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_08_034412) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "car_id"
+    t.string "name"
     t.index ["car_id"], name: "index_users_on_car_id"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
