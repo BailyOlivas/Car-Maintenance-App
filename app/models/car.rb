371a1792c92c1c8218708d3f223e83446e35733c
BailyOlivas <bailyolivas8@gmail.com>
2025-03-12 00:56:35 -0500

    Add car validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,7 +1,11 @@
 class Car < ApplicationRecord
-belongs_to :user
+belongs_to :user, optional: true
 
+validates :make, presence: true
+validates :model, presence: true
+validates :year, presence: true, numericality: { only_integer: true }
 validates :oil_change_interval, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
+validates :last_oil_change, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
 
 
 def oil_change_due?
