bbfac48c4c10b634238b85be7b9decd7534369ee
BailyOlivas <bailyolivas8@gmail.com>
2025-03-21 07:21:29 -0500

    Added 2 callbacks for maintenance record

--- app/models/maintenance_record.rb
+++ app/models/maintenance_record.rb
@@ -4,4 +4,22 @@ class MaintenanceRecord < ApplicationRecord
 
   enum :service_type, oil_change: 0, brake_inspection: 1
   validates :service_date, presence: true
+
+  after_create :update_car_odometer, :update_car_last_oil_change
+
+
+  private
+
+  def update_car_odometer
+    if car.odometer.nil? || miles > car.odometer
+      car.update(odometer: miles)
+    end
+  end
+
+
+  def update_car_last_oil_change
+    if oil_change?
+      car.update(last_oil_change: miles)
+    end
+  end
 end
